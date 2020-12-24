1d168a3c9f043db0935c73ee62b4757677199002
Brickkkkkk <buaawzz@hotmail.com>
2020-12-24 15:05:04 +0800

    message

--- code/teaching_app/config.py
+++ code/teaching_app/config.py
@@ -2,5 +2,5 @@
 Configuration parameters
 """
 
-MONGO_DB_STR = 'mongodb+srv://brick:wang0410@cluster0.djwrs.mongodb.net/Cluster0?retryWrites=true&w=majority'             # CHANGE_THIS
-SECRET_KEY = 'brick'    # CHANGE_THIS
+MONGO_DB_STR = ""             # CHANGE_THIS
+SECRET_KEY = ""    # CHANGE_THIS
